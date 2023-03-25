09f09ca4fe0724d13037f85ae58d9eca8d6bb852
seagleax <litle.project@gmail.com>
2023-03-25 14:40:53 +0700

    fix layout

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -20,6 +20,10 @@
           active: true,
           id: params
         }
+      },
+
+      handlePage() {
+
       }
     },
 
@@ -30,7 +34,12 @@
         if (updated.id.toLowerCase() !== 'all') {
           this.localProjects = this.localProjects.filter((item) => item.type.toLowerCase() === updated.id.toLowerCase())
         }
-      }
+      },
+    },
+
+    mounted() {
+      const projectProps = this.projects;
+      this.localProjects = projectProps.reverse()
     },
 
     data() {
@@ -51,7 +60,10 @@
         REDUX: 'redux.svg',
         VUEJS: 'vuejs.svg',
         LARAVEL: 'laravel.svg',
-        MARIADB: 'mariadb.svg'
+        MARIADB: 'mariadb.svg',
+        CSS: 'css.png',
+        MONGODB: 'mongodb.svg',
+        NEXTJS: 'nextjs.svg'
       };
 
       const tabs = [
@@ -64,7 +76,8 @@
       return {
         type,
         tabs,
-        localProjects: this.projects,
+        page: 1,
+        localProjects: [],
         label: {
           active: false,
           id: '',
@@ -79,45 +92,45 @@
 </script>
 
 <template>
-  <div class="pt-20 pb-10 text-dark font-bold uppercase flex-col items-center text-xl flex justify-center">
-    <div class="bg-yellow-400 w-60 h-4 relative"></div>
-    <div class="absolute mb-4">Project Histories</div>
+  <div class="pt-10 pb-5 text-dark font-bold uppercase flex-col items-center text-xl flex justify-center">
+    <div class="bg-yellow-400 w-40 md:w-60 h-2 md:h-4 relative"></div>
+    <div class="absolute mb-2 md:mb-4 md:text-lg text-sm">Project Histories</div>
   </div>
-  <div class="projects flex flex-col gap-7 mb-5">
-    <div class="flex justify-center gap-10 py-5 text-slate-500 font-semibold" id="tab-nav">
+  <div class="projects flex flex-col gap-5 md:gap-7 mb-5">
+    <div class="flex justify-center gap-5 md:gap-10 py-3 md:py-5 text-slate-500 font-semibold" id="tab-nav">
       <div
         v-for="(item, index) in tabs"
         :key="index.toString()"
         class="cursor-pointer"
-        :class="{ 'border-b-4 border-yellow-400': tab.active && (tab.id === item) }"
+        :class="{ 'border-b-2 md:border-b-4 border-yellow-400': tab.active && (tab.id === item) }"
         @click="handleTab(item)"
       >
         {{ item }}
       </div>
     </div>
-    <div class="mx-auto grid grid-cols-4 gap-7">
+    <div class="mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-5 md:gap-7">
       <div
         v-for="(project, index) in localProjects"
         :key="index.toString()"
         v-on:mouseover="handleLabel(project.name)"
         v-on:mouseleave="handleLabel('')"
-        class="shadow-lg w-60 flex flex-col rounded-xl pb-3 gap-2 border-t-2 border-neutral-100 h-[350px] filter grayscale hover:filter-none cursor-pointer"
+        class="shadow-lg w-full sm:w-80 md:w-60 flex flex-col rounded-xl pb-3 gap-2 border-t-2 border-neutral-100 h-[300px] md:h-[350px] filter grayscale hover:filter-none cursor-pointer"
       >
-        <div class="card-image flex relative flex-1 p-2 border-b-4 border-neutral-100">
-          <img class="self-center w-full p-3" :src="`/assets/images/logo/${project.image}`" :alt="project.name" />
+        <div class="card-image flex relative flex-1 p-2 border-b-2 md:border-b-4 border-neutral-100">
+          <img class="self-center w-full p-2 sm:p-3" :src="`/assets/images/logo/${project.image}`" :alt="project.name" />
         </div>
         <div
           v-if="label.active && label.id === project.name"
-          class="bg-yellow-300 text-sm px-5 py-1 font-medium rounded-md absolute mt-3 ml-[-8px]"
+          class="bg-yellow-300 text-sm px-3 py-1 font-medium rounded-md absolute mt-2 md:mt-3 ml-[-6px] md:ml-[-8px]"
         >
           {{ project.name }}
         </div>
-        <div class="py-0 w-60 overflow-auto">
-          <div class="px-4 flex flex-row gap-3 items-center">
+        <div class="py-0 w-full sm:w-80 md:w-60 overflow-auto">
+          <div class="px-2 sm:px-4 flex flex-row gap-2 sm:gap-3 items-center">
             <img
               v-for="(tech, idx) in project.tech"
               :alt="tech"
-              :width="40"
+              :width="30"
               :key="idx.toString()"
               :src="`/assets/images/stack/${type[tech]}`"
             />
