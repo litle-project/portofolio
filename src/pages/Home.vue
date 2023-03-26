150fac2afa5cc8014c688c84eb60079bff340822
seagleax <litle.project@gmail.com>
2023-03-27 03:07:36 +0700

    add company history

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,15 @@
 <script setup>
   import Banner from '@/components/Banner.vue'
   import Project from '@/components/Project.vue'
+  import Company from '@/components/Company.vue'
   import projectCollections from '/public/assets/collections/projects.json'
+  import companyCollections from '/public/assets/collections/companies.json'
 </script>
 
 <template>
   <Banner />
   <Project :projects="projectCollections" />
+  <Company :companies="companyCollections" />
 </template>
 
 <style>
