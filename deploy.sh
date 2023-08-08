e51cb12d6c386b92aa8c9c9a097e3d6c0543018b
seagleax <litle.project@gmail.com>
2023-08-08 12:21:02 +0700

    fix deploy sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:litle.project/personal-portofolio.git master:gh-pages
+git push -f git@github.com:litle-project/portofolio.git master:gh-pages
 
 cd -
